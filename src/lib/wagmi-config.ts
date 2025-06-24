0db6b4ea184d1f83758ee16b9c3e91b3d6ec927d
AslamSDM <aslamprpd@gmail.com>
2025-06-25 00:53:57 +0530

    project id update

--- src/lib/wagmi-config.ts
+++ src/lib/wagmi-config.ts
@@ -6,7 +6,7 @@ import type { AppKitNetwork } from "@reown/appkit/networks";
 
 // Get projectId from https://cloud.reown.com
 export const projectId =
-  process.env.NEXT_PUBLIC_PROJECT_ID || "b56e18d47c72ab683b10814fe9495694"; // this is a public projectId only to use on localhost
+  process.env.NEXT_PUBLIC_PROJECT_ID || "70a53b1a23f2f0f2815353fd11fd49d1"; // this is a public projectId only to use on localhost
 
 if (!projectId) {
   throw new Error("Project ID is not defined");
